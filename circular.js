c76f076e0c027eed9f065ab1e403a74ce04a4657
Jeff Jessie <jejess6125@gmail.com>
2020-10-24 11:05:25 -0500

    '

--- circular.js
+++ circular.js
@@ -1,21 +1,3 @@
-// const arrayLoop = (arr, numberOfLoops) => {
-//     let i = 0;
-//     while (i < numberOfLoops) {
-//       for (const idx in arr) {
-//         console.log(arr[idx]);
-//       }
-//       console.log("--------");
-//       i++;
-//     }
-//   };
-
-
-
-//Use one for loop or one while loop
-// Must use Modulus (%) operator
-//console.log every element and break iterations by a ----
-
-
 const math = [
     'sucks', 'really sucks', 'dreadful', 'hate algorithms',
     'sucks', 'really sucks', 'dreadful', 'hate algorithms',
@@ -23,13 +5,9 @@ const math = [
     'sucks', 'really sucks', 'dreadful', 'hate algorithms',
     'sucks', 'really sucks', 'dreadful', 'hate algorithms'];
 
-const circularArray = (arr, numberOfLoops) => {
-    let i = 0;
-    while(i < (arr.length % numberOfLoops)){
-        console.log(i);
-        console.log("-------");
-        i++;
-    }
+const circleArray = (array, numberOfLoops) => {
+let i = 0;
+while(i < (array.length * numberOfLoops) {
+console.log(i);
+}
 };
-
-circularArray(math,8);
